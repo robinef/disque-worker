123fe840392b30d6a3d4557ea90f27b35ea85e74
Frederic ROBINET <robinef@gmail.com>
2015-05-27 15:32:09 +0000

    Adding stop worker method

--- test/worker_example.js
+++ test/worker_example.js
@@ -6,7 +6,7 @@ var Worker = require('../');
 var test = new Worker('test', '127.0.0.1', 7711);
 
 //Waiting for a job on queue and execute callback
-test.getJob('queue', 1, function(res){
+test.getJob('urlTester', 10, function(res){
   console.log('Hi from callback');
   console.log(res);
 });
